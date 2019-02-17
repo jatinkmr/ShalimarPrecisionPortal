898625692e38e52122e11751c938d4e824bd20a0
jatinkmr <kumar.jatin873@gmail.com>
2019-02-17 21:39:12 +0530

    EnquiryInsertUpdateDelete

--- src/java/EnquiryOperation.java
+++ src/java/EnquiryOperation.java
@@ -1,47 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
+import java.io.*;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author jatin kamboj
- */
+import javax.servlet.http.*;
+import java.sql.*;
 @WebServlet(urlPatterns = {"/EnquiryOperation"})
 public class EnquiryOperation extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet EnquiryOperation</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet EnquiryOperation at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String action,id,typ,ques,ans;
+            action = request.getParameter("enqrsb");
+            System.out.println("Your Action : "+action);
+            id = request.getParameter("eid");
+            typ = request.getParameter("etyp");
+            ques = request.getParameter("eques");
+            ans = request.getParameter("eans");            
+            
+            try{
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                Connection conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","shalimaresol","shalimaresol");
+                Statement stmt=conn.createStatement();           
+                if(action.equals("Insert")){
+                    if((!"".equals(id))&&(!"".equals(typ))&&(!"".equals(ques))&&(!"".equals(ans))){
+                        int a = stmt.executeUpdate("insert into enquiry values('"+id+"','"+typ+"','"+ques+"','"+ans+"')");
+                        if(a > 0){
+                            request.setAttribute("errmsg","Record Inserted Successfully !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Enquiry.jsp");
+                            rsd.forward(request, response);
+                        }else{
+                            request.setAttribute("errmsg","Record Doesn't Inserted !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Enquiry.jsp");
+                            rsd.forward(request, response);                        
+                        }
+                    }else{
+                        request.setAttribute("errmsg","Some of the Given Fields is Empty !");
+                        RequestDispatcher rsd = request.getRequestDispatcher("/Enquiry.jsp");
+                        rsd.forward(request,response);
+                    }                    
+                }else if(action.equals("Update")){
+                    if((!"".equals(id))&&(!"".equals(typ))&&(!"".equals(ques))&&(!"".equals(ans))){
+                        int a = stmt.executeUpdate("update enquiry set c_type='"+typ+"' ,c_ques='"+ques+"',c_ans='"+ans+"'  where c_id='"+id+"' ");
+                        if(a > 0){
+                            request.setAttribute("errmsg","Record Updated Successfully !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Enquiry.jsp");
+                            rsd.forward(request, response);
+                        }else{
+                            request.setAttribute("errmsg","Record Doesn't Updated !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Enquiry.jsp");
+                            rsd.forward(request, response);                        
+                        }
+                    }else{
+                        request.setAttribute("errmsg","Some of the Given Fields is Empty !");
+                        RequestDispatcher rsd = request.getRequestDispatcher("/Enquiry.jsp");
+                        rsd.forward(request, response);                        
+                    }                    
+                }else if(action.equals("Delete")){
+                    if(!"".equals(id)){
+                        int a = stmt.executeUpdate("delete from enquiry where c_id='"+id+"'");
+                        if(a > 0){
+                            request.setAttribute("errmsg","Record Deleted Successfully !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Enquiry.jsp");
+                            rsd.forward(request, response);
+                        }else{
+                            request.setAttribute("errmsg","Record Doesn't Deleted !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Enquiry.jsp");
+                            rsd.forward(request, response);                        
+                        }
+                    }else{
+                        request.setAttribute("errmsg","Please Fill The Enquiry ID First !");
+                        RequestDispatcher rsd = request.getRequestDispatcher("/Enquiry.jsp");
+                        rsd.forward(request, response);
+                    }                
+                }else if(action.equals("Search")){
+                
+                }
+            }catch(Exception ex){
+                request.setAttribute("errmsg","An Error Occurred While Establishing The Connection!!");
+                RequestDispatcher rsd = request.getRequestDispatcher("/Enquiry.jsp");
+                rsd.forward(request, response);
+            }
         }
     }
 
