f6c933492d692471ef70da060e9a1ab33586afd1
jatinkmr <kumar.jatin873@gmail.com>
2019-02-19 20:56:55 +0530

    Registration Operation

--- src/java/RegistrationOperation.java
+++ src/java/RegistrationOperation.java
@@ -1,84 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
+import java.io.*;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author jatin kamboj
- */
-@WebServlet(urlPatterns = {"/RegistrationOperation"})
+import javax.servlet.http.*;
+import java.sql.*;
+@WebServlet(urlPatterns = {"/RegistraionOperation"})
 public class RegistrationOperation extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RegistrationOperation</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RegistrationOperation at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String action,usrid,usrtp,usrnm,upass1,upass2,sques,san,con;
+            action = request.getParameter("sgn");
+            usrid = request.getParameter("uid");
+            usrtp = request.getParameter("usrtyp");
+            upass1 = request.getParameter("upsswd1");
+            upass2 = request.getParameter("upsswd2");
+            sques = request.getParameter("secques");
+            san = request.getParameter("sans");
+            con = request.getParameter("ucon");            
+            usrnm = request.getParameter("unm");
+            
+            try{
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                Connection conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","shalimaresol","shalimaresol");
+                Statement stmt=conn.createStatement();                     
+                if((!"".equals(usrid))&&(!"".equals(usrtp))&&(!"".equals(usrnm))&&(!"".equals(upass1))&&(!"".equals(upass2))&&(!"".equals(sques))&&(!"".equals(san))&&(!"".equals(con))){
+                    if(upass1.equals(upass2)){
+                        int a = stmt.executeUpdate("insert into userreg values('"+usrid+"','"+usrtp+"','"+upass1+"','"+sques+"','"+san+"','"+con+"','"+usrnm+"')");
+                        if(a > 0){
+                            request.setAttribute("errmsg","User Created Successfully !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/UserReg.jsp");
+                            rsd.forward(request, response);
+                        }else{
+                            request.setAttribute("errmsg","User Doesn't Created !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/UserReg.jsp");
+                            rsd.forward(request, response);                        
+                        }
+                    }else{
+                        request.setAttribute("errmsg","Password Didn't Match !");
+                        RequestDispatcher rsd = request.getRequestDispatcher("/UserReg.jsp");
+                        rsd.forward(request, response);
+                    }
+                }else{
+                    request.setAttribute("errmsg","Some of the Given Fields is Empty !");
+                    RequestDispatcher rsd = request.getRequestDispatcher("/UserReg.jsp");
+                    rsd.forward(request, response);
+                }
+            }catch(Exception ex){
+                request.setAttribute("errmsg","An Error Has Been Occurred While Creating A New User !");
+                RequestDispatcher rsd = request.getRequestDispatcher("/UserReg.jsp");
+                rsd.forward(request, response);
+            }
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
