fa6738786eb904921c763b9fc95a18a10663616a
jatinkmr <kumar.jatin873@gmail.com>
2019-02-19 08:50:50 +0530

    Promotion Insert Update Delete

--- src/java/PromotionOperation.java
+++ src/java/PromotionOperation.java
@@ -1,47 +1,86 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
+import java.io.*;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- *
- * @author jatin kamboj
- */
+import javax.servlet.http.*;
+import java.sql.*;
 @WebServlet(urlPatterns = {"/PromotionOperation"})
 public class PromotionOperation extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PromotionOperation</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PromotionOperation at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String action,prmid,prnnm,prprc,prpid;
+            action = request.getParameter("prmsb");
+            System.out.println("Your Action : "+action);
+            prmid = request.getParameter("pid");
+            prnnm = request.getParameter("pnm");
+            prprc = request.getParameter("pprc");
+            prpid = request.getParameter("prid");
+            
+            try{
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+                Connection conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","shalimaresol","shalimaresol");
+                Statement stmt=conn.createStatement(); 
+                if(action.equals("Insert")){
+                    if((!"".equals(prmid))&&(!"".equals(prnnm))&&(!"".equals(prprc))&&(!"".equals(prpid))){
+                        int a = stmt.executeUpdate("insert into promotion values ('"+prmid+"','"+prnnm+"','"+prprc+"','"+prpid+"') ");
+                        if(a > 0){
+                            request.setAttribute("errmsg","Record Inserted Successfully !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Promotion.jsp");
+                            rsd.forward(request, response);                                                    
+                        }else{
+                            request.setAttribute("errmsg","Record Doesn't Inserted !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Promotion.jsp");
+                            rsd.forward(request, response);                        
+                        }
+                    }else{
+                        request.setAttribute("errmsg","Some of the Given Fields is Empty !");
+                        RequestDispatcher rsd = request.getRequestDispatcher("/Promotion.jsp");
+                        rsd.forward(request, response);                        
+                    }                
+                }else if(action.equals("Update")){
+                    if((!"".equals(prmid))&&(!"".equals(prnnm))&&(!"".equals(prprc))&&(!"".equals(prpid))){
+                        int a = stmt.executeUpdate("update promotion set pr_name='"+prnnm+"' ,pr_prc='"+prprc+"' ,pro_id='"+prpid+"'  where pr_id='"+prmid+"' ");
+                        if(a > 0){
+                            request.setAttribute("errmsg","Record Successfully Updated !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Promotion.jsp");
+                            rsd.forward(request, response);                        
+                        }else{
+                            request.setAttribute("errmsg","Record Doesn't Updated !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Promotion.jsp");
+                            rsd.forward(request, response);                        
+                        }
+                    }else{
+                        request.setAttribute("errmsg", "Some of the Given Fields is Empty !");
+                        RequestDispatcher rsd = request.getRequestDispatcher("/Promotion.jsp");
+                        rsd.forward(request, response);                    
+                    }                         
+                }else if(action.equals("Delete")){
+                    if(!"".equals(prmid)){
+                        int a = stmt.executeUpdate("delete from promotion where pr_id='"+prmid+"' ");
+                        if(a > 0){
+                            request.setAttribute("errmsg","Record Successfully Deleted !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Promotion.jsp");
+                            rsd.forward(request, response);                    
+                        }else{
+                            request.setAttribute("errmsg","Record Doesn't Deleted !");
+                            RequestDispatcher rsd = request.getRequestDispatcher("/Promotion.jsp");
+                            rsd.forward(request, response);                    
+                        }
+                    }else{
+                        request.setAttribute("errmsg","Plesae Fill The Promotion ID !");
+                        RequestDispatcher rsd = request.getRequestDispatcher("/Promotion.jsp");
+                        rsd.forward(request, response);                    
+                    }                
+                }else if(action.equals("Search")){
+                
+                }
+                
+            }catch(Exception ex){
+                request.setAttribute("errmsg","An Error Occurred While Establishing The Connection !");
+                RequestDispatcher rsd = request.getRequestDispatcher("/Promotion.jsp");
+                rsd.forward(request, response);
+            }
         }
     }
 
